8a78cc393e5cc9bced3c1cc465c7dd44d087c49c
Thibaut Montanier <thibaut.montanier@gmail.com>
2024-11-13 11:14:43 +0100

    feat: ReactiveForms et formGroup

--- src/app/components/player-form/player-form.component.ts
+++ src/app/components/player-form/player-form.component.ts
@@ -1,20 +1,65 @@
 import { CommonModule } from '@angular/common';
-import { Component, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, Input, OnChanges, output, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Player } from '../../models/player';
 
 @Component({
   selector: 'app-player-form',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './player-form.component.html',
   styleUrl: './player-form.component.scss'
 })
-export class PlayerFormComponent {
-  public currentPlayer = new Player();
+export class PlayerFormComponent  implements OnChanges {
+
+  @Input() editPlayer: Player|undefined;
   public addPlayer = output<Player>();
+
+  public formGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+    firstName: new FormControl(''),
+    ranking: new FormControl(0, [Validators.required, Validators.min(1)])
+  });
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['editPlayer'] && this.editPlayer != null){
+      // affectation des valeurs de editPlayer au formulaire
+      this.formGroup.setValue(this.editPlayer);
+    }
+  }
+
+  public onNew(){
+    // reset des valeurs du formulaire
+    this.formGroup.reset();
+  }
+
   public onSubmit(){
-    this.addPlayer.emit(this.currentPlayer);
-    this.currentPlayer = new Player();
+    const result = new Player();
+    result.name = this.formGroup.value.name!;
+    result.firstName = this.formGroup.value.firstName!;
+    result.ranking = +this.formGroup.value.ranking!;
+    this.addPlayer.emit(result);
+  }
+
+
+  public showFieldError(fieldName: string, errorKind: string = ''): boolean{
+    const formControl =this.formGroup.get(fieldName);
+    return formControl != null && formControl.touched && !formControl.valid;
+  }
+  // version avancée de la gestion d'erreur
+  // public showFieldError(fieldName: string, errorKind: string = ''): boolean{
+  //   if (this.formGroup.get(fieldName)?.touched) {
+  //     if (errorKind == ''){
+  //       return !this.formGroup.get(fieldName)?.valid;
+  //     }else{
+  //       return this.formGroup.get(fieldName)?.errors?.[errorKind] != null;
+  //     }
+  //   }else{
+  //     return false;
+  //   }
+  // }
+
+  getError(fieldName: string){
+    return this.formGroup.get(fieldName)?.errors;
   }
 }
